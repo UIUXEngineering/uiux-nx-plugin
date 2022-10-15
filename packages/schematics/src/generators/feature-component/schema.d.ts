ddd9c1fcafc08f1ba6fd752c05f738ba7dbc1668
jerryorta.dev@gmail.com <jerryorta.dev@gmail.com>
2022-10-15 06:08:29 -0500

    feat(schematics): stub

---
+++ packages/schematics/src/generators/feature-component/schema.d.ts
@@ -0,0 +1,5 @@
+export interface FeatureComponentGeneratorSchema {
+    name: string;
+    tags?: string;
+    directory?: string;
+}
---
+++ packages/schematics/src/generators/ngrx-effect-store/schema.d.ts
@@ -0,0 +1,5 @@
+export interface NgrxEffectStoreGeneratorSchema {
+    name: string;
+    tags?: string;
+    directory?: string;
+}
